dc0043d21eec1fabfc3591286ccdf829a59af507
Erickson Powell <ericksonpowell@gmail.com>
2021-03-27 00:19:22 -0700

    contact page complete

--- src/App.js
+++ src/App.js
@@ -8,21 +8,31 @@ import Home from "./components/pages/Home";
 import AboutMe from "./components/pages/AboutMe";
 import Portfolio from "./components/pages/Portolio";
 import ContactMe from "./components/pages/ContactMe";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { green } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  }
+});
 
 function App() {
   return (
-    <Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/AboutMe" component={AboutMe} />
-          <Route exact path="/Portfolio" component={Portfolio} />
-          <Route exact path="/ContactMe" component={ContactMe} />
-          {/* <Route default path="*" component={Home} /> */}
-        </Switch>
-      </Router>
-      <Footer />
-    </Fragment>
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/AboutMe" component={AboutMe} />
+            <Route exact path="/Portfolio" component={Portfolio} />
+            <Route exact path="/ContactMe" component={ContactMe} />
+            <Route default path="*" component={Home} />
+          </Switch>
+        </Router>
+        <Footer />
+      </Fragment>
+    </ThemeProvider>
   );
 }
 
--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -1,11 +1,79 @@
+import { Box, Button, ButtonGroup, Container, Grid, IconButton, TextField, Typography } from "@material-ui/core";
 import { Fragment } from "react";
-import Footer from "../layout/Footer";
 import Header from "../layout/Header";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
 
 const ContactMe = () => {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <Fragment>
       <Header title={"Contact Me"}/>
+      <br /><br />
+      <form action="https://formspree.io/f/moqyayaz" method="POST">
+        <Grid container>
+          <Grid item xs={1} sm={2} />
+          <Grid item container xs={10} sm={8} spacing={3}>
+            <Grid item xs={12}>
+              <Box display="flex" justifyContent="center">
+                <ButtonGroup>
+                  <Button variant="text" href="https://www.github.com/epowelldev">Github</Button>
+                  <Button variant="text" disabled>777-777-7777</Button>
+                  <Button variant="text" href="https://www.linkedin.com/in/erickson-powell-30a52b79/">LinkedIn</Button>
+                </ButtonGroup>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="contact-name"
+                label="Name"
+                name="contact-name"
+                placeholder="Name"
+                variant="outlined"
+                InputLabelProps={{ shrink: true, }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="contact-email"
+                label="Email"
+                name="contact-email"
+                placeholder="Email"
+                variant="outlined"
+                InputLabelProps={{ shrink: true, }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="contact-message"
+                label="Message"
+                name="contact-message"
+                placeholder="Message"
+                variant="outlined"
+                InputLabelProps={{ shrink: true, }}
+                multiline
+                rows={6}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={8} sm={10} />
+            <Grid item xs={3} sm={1} >
+              <Button variant="text" type="submit" onSubmit={handleSubmit} >Submit</Button>
+            </Grid>
+            <Grid item xs={1} sm={1} />
+          </Grid>
+          <Grid item xs={1} sm={2} />
+        </Grid>
+      </form>
     </Fragment>
   );
 };
